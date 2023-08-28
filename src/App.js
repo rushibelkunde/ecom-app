51d2f6f8da9a9b9d0404f3da873558f22f2385b9
rushibelkunde <rushibelkunde18@gmail.com>
2023-08-28 15:38:07 +0530

    db edit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+
+  useEffect(()=>{
+
+    const fetchData = async ()=>{
+      const res = await fetch("https://my-json-server.typicode.com/rushibelkunde/ecom-app/db")
+
+    const data = await res.json()
+    console.log(data)
+
+    }
+    fetchData()
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
