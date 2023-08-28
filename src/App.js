ca768ef1e9ba3035566adc304c5bcaa2c9e46e8f
rushibelkunde <rushibelkunde18@gmail.com>
2023-08-28 15:59:07 +0530

    update db

--- src/App.js
+++ src/App.js
@@ -8,7 +8,14 @@ function App() {
   useEffect(()=>{
 
     const fetchData = async ()=>{
-      const res = await fetch("https://my-json-server.typicode.com/rushibelkunde/ecom-app/db")
+      const res = await fetch("https://my-json-server.typicode.com/rushibelkunde/ecom-app/products",{
+        method: "POST",
+        body : JSON.stringify({id: 101, name: "Rushikesh"}),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        }
+      })
+      console.log(res)
 
     const data = await res.json()
     console.log(data)
@@ -18,6 +25,8 @@ function App() {
   },[])
   return (
     <div className="App">
+
+      <h1>App</h1>
       
     </div>
   );
